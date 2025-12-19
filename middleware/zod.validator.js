05ebb2d0aae1054925724d7207fbd284c2005de2
Dip908 <dipchondo007@gmail.com>
2025-12-19 15:25:02 +0600

    add validator middleware before adding a department

--- middleware/zod.validator.js
+++ middleware/zod.validator.js
@@ -5,14 +5,17 @@ export const validator = (schema) => {
     return (req, res, next) => {
         try {
             const result = schema.safeParse(req.body);
+            console.log(result)
             if (result.success) {
                 next()
             }else if (result.error instanceof z.ZodError) {
-                // console.log(result.error.issues[0].message)
+
+                console.log(result.error.issues[0].message)
                 res.json({ message: "Invalid Information. Double Check Before Submit Data" })
             }
 
         } catch (error) {
+            console.log(error)
             if (result.error instanceof z.ZodError) {
                 res.json({ message: "Invalid Information. Double Check Before Submit Data" })
             }
--- routes/dep.route.js
+++ routes/dep.route.js
@@ -1,8 +1,10 @@
 import express from 'express' 
 import { newDepartment } from '../controller/departmet.controller.js'
+import { validator } from '../middleware/zod.validator.js'
+import { departmentSchema } from '../schema/department.schema.js'
 
 const router=express.Router()
 
-router.post('/add-depertment',newDepartment)
+router.post('/add-depertment',validator(departmentSchema), newDepartment)
 
 export default router
